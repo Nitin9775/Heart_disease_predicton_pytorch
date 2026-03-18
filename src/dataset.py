91b916b7035a2b02ae3d3d7acb505f531d56bbcd
Nitin Sharma <nitin.20230035@mnnit.ac.in>
2026-03-19 02:05:14 +0530

    ...

--- src/dataset.py
+++ src/dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 class HeartDiseaseDataset(Dataset):
     def __init__(self, X, y):
         self.X = torch.tensor(X, dtype=torch.float32) # Convert features to PyTorch tensor
-        self.y = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)   # Convert target variable to PyTorch tensor
+        self.y = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)   # Convert target variable to PyTorch tensor and this view(-1, 1) is used to reshape the target variable into a column vector, which is necessary for binary classification tasks where the output layer has a single neuron.
 
     def __len__(self):
         return len(self.X)  # Return the number of samples in the dataset
